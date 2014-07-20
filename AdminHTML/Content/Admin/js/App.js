3faeb1183f2760ed5843c7da92b5db7ea5000f0a
doubletong <twotong@gmail.com>
2014-07-20 21:38:18 +0800

    test

---
+++ AdminHTML/Content/Admin/js/App.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+
+    $(".down-nav>a").click(function (e) {
+        e.preventDefault();
+        var n = $(this).next("ul");
+        if (n.hasClass('nav-open')) {
+
+            n.animate({ height: 0 }, 300);
+            setTimeout(function () {
+                n.removeClass('nav-open');
+            },300);            
+        }
+        else {
+            var newH = n.css('height', 'auto').height();
+         
+            n.height(0).animate({ height:newH }, 300);
+            setTimeout(function () {
+                n.addClass('nav-open');
+            },300);
+           
+        }
+    });
+});
